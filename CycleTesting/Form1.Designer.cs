3b7b575c6e687060bbd4f9dc2a2cd659d769734c
Luke Edgecombe <145694364+Luk446@users.noreply.github.com>
2024-03-27 09:14:57 +0000

    Add project files.

---
+++ CycleTesting/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CycleTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(334, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(446, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.numericUpDown1.Location = new System.Drawing.Point(497, 251);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            180,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 38);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(634, 54);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Duty Cycle Testing Program";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(328, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Duration:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(355, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Important: set duration before beginning\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(623, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 36);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "(seconds)";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(334, 408);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(446, 43);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 572);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ CycleTesting/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace CycleTesting
+{
+    // Define variables for storing data
+    public partial class Form1 : Form  
+    {
+        private string csvFilePath; // define string for csv upload 
+        // declare the vars for lists for pv and tv values (not init yet)
+        private List<int> pvValues;
+        private List<int> tvValues;
+
+        // create the timers 
+        private Timer timer; // 'simple' timer
+        private System.Windows.Forms.Timer progressTimer = new System.Windows.Forms.Timer();
+        // in second case, a different type of timer is used that is more suitable for constant updating 
+
+
+        // create the form execute certain preliminary blocks + functions
+        public Form1()
+        {
+            InitializeComponent(); // create
+            // now they are instantiated using 'new' which allocates memory and opens empty list
+            pvValues = new List<int>();
+            tvValues = new List<int>();
+
+            InitializeTimer(); // init the primary timer 
+            progressTimer.Tick += new EventHandler(progressTimer_Tick); // attach event handler to tick for bar timer
+        }
+
+        // Timer for the bar tick event 
+        // includes cond. state for stopping when finished
+        private void progressTimer_Tick(object sender, EventArgs e)
+        {
+            // Increment progress bar
+            if (progressBar1.Value < progressBar1.Maximum)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                // Stop when max val
+                progressTimer.Stop();
+            }
+        }
+
+        // Initialise the serial port and define the params 
+        private void InitializeSerialPort()
+        {
+            serialPort = new SerialPort(); // create 
+            serialPort.PortName = "COM3"; // set the port ( fix later to rid hard code!!)
+            serialPort.BaudRate = 9600; // set speed of transmission ( the same as set in Arduino IDE)
+            serialPort.DataReceived += SerialPort_DataReceived; // subsribe event to handler that processes
+            serialPort.Open(); // begin data transmission
+        }
+
+        // Create object for starting timer (primary timer)
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+            // note - interval value defined elsewhere for updown val
+        }
+
+        // data reception and processing block
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string incomingData = serialPort.ReadLine(); // read each line
+
+            // Due to complex data parse for data with approriate values
+            if (incomingData.StartsWith("PV: ")) // check for "PV: "
+            {
+                string dataValue = incomingData.Substring(4); // we need to ignore first 4 points so that the identifier isnt counted
+                // here we use int.TryParse to negate crashing, if for any reason the value was not an integer (string etc)
+                // then it could cause the application to freeze/crash so this way the data collection can continue and ignore error
+                if (int.TryParse(dataValue, out int potValue)) 
+                {
+                    pvValues.Add(potValue); // add to the predetermined string the integer value
+                }
+            }
+            // secondary case for the other value present in monitor
+            else if (incomingData.StartsWith("TV: "))
+            {
+                string dataValue = incomingData.Substring(4); 
+                if (int.TryParse(dataValue, out int tvValue))
+                {
+                    tvValues.Add(tvValue);
+                }
+            }
+        }
+
+        // Once the 'tick' event is completed this is called which stops the data collection
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop(); // stop timer
+            serialPort.Close(); // close port 
+            SaveDataToCSV(); // execute data save
+            MessageBox.Show("Data collection completed and saved."); // inform user
+        }
+
+        // create a method for writing the data to csv
+        // for the magnitude of data collected (depending on what string) execute a number of loops of line writing 
+        private void SaveDataToCSV()
+        {
+            // before saving data check:
+            // ensure the file path is legitimate ( not empty)
+            // ensure thay data has actually been recorded ( likely result of serial port wire issue )
+            // these checks like others before prevent crashes and the storing of wrong data
+            if (!string.IsNullOrEmpty(csvFilePath) && (pvValues.Count > 0 || tvValues.Count > 0))
+            {
+                using (StreamWriter sw = new StreamWriter(csvFilePath)) // uses .NET library function
+                {
+                    // Define headers ( this is to simplify python data analysis in future )
+                    sw.WriteLine("PV,TV");
+                    // check how many items are both strings and then loop for max of that
+                    for (int i = 0; i < Math.Max(pvValues.Count, tvValues.Count); i++)
+                    {
+                        // define a variable for the values but also:
+                        // like most seemingly over complicated stuff its here to avoid error
+                        // this ensures proper aligment of data in the csv, basically if the value is bounded in the i range
+                        // it will attach to var, if not, the string will be empty 
+                        // sequence var = is i smaller than the amount ? then if so attach value to the var
+                        // TLDR: stops csv aligment screwing up
+                        var pvValue = i < pvValues.Count ? pvValues[i].ToString() : "";
+                        var tvValue = i < tvValues.Count ? tvValues[i].ToString() : "";
+
+                        // print line 
+                        sw.WriteLine($"{pvValue},{tvValue}");
+                    }
+                }
+            }
+        }
+
+        // Handles what happens when the Record button is pressed
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // using .NET library for prompt
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv"; // user can only save as csv not anything else
+                saveFileDialog.Title = "Save the CSV File"; // Set window title
+                saveFileDialog.ShowDialog(); // display configured window
+
+
+                if (!string.IsNullOrEmpty(saveFileDialog.FileName)) // checks if user has selected the file path
+                {
+                    csvFilePath = saveFileDialog.FileName; // instantiate file path
+
+                    // cleanse the inputs
+                    pvValues.Clear();
+                    tvValues.Clear();
+
+                    // begin data collection!
+                    InitializeSerialPort();
+
+                    // Set the timer interval based on the numericUpDown1 value
+                    timer.Interval = (int)numericUpDown1.Value * 1000; // C# reads time in ms so convert to s
+                    timer.Start(); // Start the timer
+                    MessageBox.Show("Recording Begun"); // inform user of starting
+
+                    // Now for the bar timer
+
+                    // Start the progress bar from min value
+                    progressBar1.Value = progressBar1.Minimum; // set the progress bar initial value
+                    int totalDuration = (int)numericUpDown1.Value * 1000; // Convert to ms
+                    int interval = totalDuration / (progressBar1.Maximum - progressBar1.Minimum); // do some maths to find the tick range/rate
+                    progressTimer.Interval = interval; // define the length of timer based on above maths
+                    progressTimer.Start(); // start timer with above params 
+                }
+                else // secondary case incase of imporoper user action
+                {
+                    MessageBox.Show("Invalid File Path");
+                }
+            }
+        }
+        // these are autogenerated objects and serve no function
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
