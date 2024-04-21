ef0bada74b59bdefea02a6090f151a015aba4bf4
Luke Edgecombe <145694364+Luk446@users.noreply.github.com>
2024-04-21 15:59:42 +0100

    Add files via upload

--- ArduinoIntegration.ino
+++ ArduinoIntegration.ino
@@ -1,3 +1,24 @@
+/* Simplified just including print section but
+
+tact2 should be attached to the fsr value
+tempmath2 is the result of the thermistor calibration calculations
+tempmath22 is the another thermistor
+angle is the result of the flexsensor angle calculation
+
+*/
+
+void setup() {
+
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+
+  // all maths and calcs for values
+
+  
   // print for debug + app linkage
   Serial.print("PV: "); // signal
   Serial.println(tact2); // fsr val
@@ -13,3 +34,7 @@
   Serial.print("AV: ");
   int angleclean = (int)angle;
   Serial.println(angleclean);
+
+  delay(100); // flooding delay
+
+}
